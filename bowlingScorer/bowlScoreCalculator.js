9ab2394dc3027fcb73289c2203d412f9abf8ca8a
yashasva01 <yashasva2001@gmail.com>
2023-01-18 12:43:25 +0530

    feat: add feature to calculate score on rolls and tests

--- bowlingScorer/bowlScoreCalculator.js
+++ bowlingScorer/bowlScoreCalculator.js
@@ -4,6 +4,18 @@ let rolls = [1,2,3,4,'5','five'];//[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 function getScore (rolls) {
     if (!Array.isArray(rolls)) {throw new Error('The Input type did not match the requirement');}
     if (rolls.some(isNaN)) {throw new Error('Some of the input are not in type number');}
+    let totalScore = 0;
+    for (let ind in rolls){
+        let currentRoll = 0;
+        let currentFrame = 0;
+        if (currentRoll <= 2) {
+            currentRoll +=1;
+            let currentScore = rolls[ind];
+            if(currentRoll === 2){
+                currentFrame += 1
+            }
+        }
+    }
 }
 
 console.log(getScore(rolls))
